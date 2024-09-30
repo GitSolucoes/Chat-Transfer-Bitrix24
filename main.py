449c0ab831c1e9cc845ae3d44cfbf275c52ed26d
gitTaynara4x4 <git.taynara.4x4@gmail.com>
2024-09-30 17:17:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,79 @@
-from flask import Flask, request, jsonify
 import requests
-import time
+import os
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+CODIGO_BITRIX = os.getenv('CODIGO_BITRIX')
 
 app = Flask(__name__)
 
-BITRIX_WEBHOOK_URL = "https://marketingsolucoes.bitrix24.com.br/rest/35002/7a2nuej815yjx5bg/bizproc.workflow.start"
+def extrair_numero(string):
+    start_index = string.index("_") + 1
+    numero = string[start_index:]
+    return numero
 
-@app.route('/webhook/workflowone', methods=['POST'])
-def workflowone():
-    return start_workflow("1196")
+# ROTA PARA TRANSFERENCIA DE BATE-PAPO NA BITRIX BATENDO COM RESPONSAVEL INDO PRA FILA
+@app.route('/change-the-chat-channel/', methods=['POST'])
+def change_the_chat_channel():
+    CONTACT_ID = request.args.get('CONTACT_ID')
+    QUEUE_ID = request.args.get('QUEUE_ID')
 
-@app.route('/webhook/workflowtwo', methods=['POST'])
-def workflowtwo():
-    return start_workflow("1200")
+    if not CONTACT_ID or not QUEUE_ID:
+        return jsonify({'error': 'CONTACT_ID and QUEUE_ID must be provided in the URL parameters'}), 400
 
-@app.route('/webhook/workflowthree', methods=['POST'])
-def workflowthree():
-    return start_workflow("1202")
-
-def start_workflow(workflow_id):
-    print("Webhook acionado!")  # Log para verificar se o endpoint está sendo chamado
-    deal_id = request.args.get('deal_id')
-    if not deal_id:
-        return jsonify({"error": "deal_id não fornecido"}), 400
-
-    array = ["crm", "CCrmDocumentDeal", f"DEAL_{deal_id}"]
-    data = {
-        "TEMPLATE_ID": workflow_id,
-        "DOCUMENT_ID": array
-    }
-
-    time.sleep(10)
-    response = requests.post(BITRIX_WEBHOOK_URL, json=data)
+    base_url = f'https://marketingsolucoes.bitrix24.com.br/rest/35002/{CODIGO_BITRIX}'
+    url = f'{base_url}/imopenlines.crm.chat.getLastId?CRM.ENTITY_TYPE=CONTACT&CRM_ENTITY={CONTACT_ID}'
     
-    return jsonify(response.json()), response.status_code
+    response = requests.post(url)
+    
+    if response.status_code == 200:
+        datajson = response.json()
+        id_chat = datajson['result']
+        
+        url2 = f'{base_url}/imopenlines.operator.transfer?CHAT_ID={id_chat}&QUEUE_ID={QUEUE_ID}'
+        response2 = requests.post(url2)
+        
+        if response2.status_code == 200:
+            return "New responsible approved"
+        else:
+            return f"No responsible approved: {response2.text}" 
+    else:
+        return f"Failed to get chat ID: {response.text}"
+
+# NOVA ROTA PARA TRANSFERENCIA DE BATE-PAPO COM RESPONSÁVEL
+@app.route('/change-the-chat-responsible/', methods=['POST'])
+def change_the_chat_responsability():
+    CONTACT_ID = request.args.get('CONTACT_ID')
+    TRANSFER_ID = request.args.get('TRANSFER_ID')
+
+    if not CONTACT_ID or not TRANSFER_ID:
+        return jsonify({'error': 'CONTACT_ID and TRANSFER_ID must be provided in the URL parameters'}), 400
+
+    TRANSFER_ID = extrair_numero(TRANSFER_ID)
+
+    base_url = f'https://marketingsolucoes.bitrix24.com.br/rest/35002/{CODIGO_BITRIX}'
+    url = f'{base_url}/imopenlines.crm.chat.getLastId?CRM.ENTITY_TYPE=CONTACT&CRM_ENTITY={CONTACT_ID}'
+    
+    response = requests.post(url)
+    
+    if response.status_code == 200:
+        datajson = response.json()
+        id_chat = datajson['result']
+        
+        url2 = f'{base_url}/imopenlines.operator.transfer?CHAT_ID={id_chat}&TRANSFER_ID={TRANSFER_ID}'
+        response2 = requests.post(url2)
+        
+        if response2.status_code == 200:
+            return "New responsible approved"
+        else:
+            return f"No responsible approved: {response2.text}" 
+    else:
+        return f"Failed to get chat ID: {response.text}"
+
+@app.route('/')
+def index():
+    return "Hello, this is the application!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7072)
+    app.run(port=7072, host='0.0.0.0')
